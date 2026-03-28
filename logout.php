2ca9929f3e881e4dd62da8b1e2b7cd36fe69af2b
HilieSaro <244202881+HilieSaro@users.noreply.github.com>
2026-03-28 11:20:36 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 // Clear the wallet cookie and redirect to the login page.
-setcookie('wallet_address', '', time() - 3600, '/');
+setcookie('feAutologin', '', time() - 3600, '/', null, !!Kwf_Config::getValue('server.https'), true);
 header('Location: Login.php');
 exit();
