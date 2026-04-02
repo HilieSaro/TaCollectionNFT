ea70a4d2d1abbb3688777705bc636925b958e547
HilieSaro <244202881+HilieSaro@users.noreply.github.com>
2026-04-02 15:24:17 +0200

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
+unset($_SESSION['wallet_address']);
+setcookie('wallet_address', '', time() - 3600, '/');
 setcookie('feAutologin', '', [
     'expires'  => time() - 3600,
     'path'     => '/',
     'domain'   => '',
-    'secure'   => isset($_SERVER['HTTPS']), // Secure si HTTPS
-    'httponly' => true,                     // HttpOnly
-    'samesite' => 'Strict'                  // Protection CSRF maximale
+    'secure'   => isset($_SERVER['HTTPS']),
+    'httponly' => true,
+    'samesite' => 'Strict'
 ]);
 
 header('Location: Login.php');
